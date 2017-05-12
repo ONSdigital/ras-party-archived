deb7c2903da9734f139612ce14aba10104ccaadd
Richard Ingram <rich.d.ingram@gmail.com>
2017-05-12 10:26:00 +0100

    Commenting in validate scope check function

--- ras-party/app.py
+++ ras-party/app.py
@@ -176,43 +176,41 @@ def validate_scope(jwt_token, scope_type):
     :return: Boolean
     """
 
-    return True
+    app.logger.info("validate_scope jwt_token: {}, scope_type: {}".format(jwt_token, scope_type))
 
-    # app.logger.info("validate_scope jwt_token: {}, scope_type: {}".format(jwt_token, scope_type))
-    #
-    # # Make sure we can decrypt the token and it makes sense
-    #
-    # return_val= False
-    # try:
-    #     decrypted_jwt_token = decode(jwt_token)
-    #     if decrypted_jwt_token['scope']:
-    #         for user_scope_list in decrypted_jwt_token['scope']:
-    #             if user_scope_list == scope_type:
-    #                 app.logger.debug('Valid JWT scope.')
-    #                 return_val=True
-    #
-    #     if not return_val:
-    #         app.logger.warning('Invalid JWT scope.')
-    #         return False
-    #
-    #     if decrypted_jwt_token['expires_at']:
-    #         # We have a time stamp so check this token has not expired
-    #         #TODO Add UTC Time stamp validation
-    #         app.logger.info('Token: {} has a UTC time stamp of: {}'.format(decrypted_jwt_token['access_token'],decrypted_jwt_token['expires_at']))
-    #     else:
-    #         # We don't have a time stamp
-    #         app.logger.warning('Token has expired for token Value: {}'.format(decrypted_jwt_token['access_token']))
-    #         return False
-    #
-    #     return return_val
-    #
-    # except JWTError:
-    #     app.logger.warning('JWT scope could not be validated.')
-    #     return False
-    #
-    # except KeyError:
-    #     app.logger.warning('JWT scope could not be validated.')
-    #     return False
+    # Make sure we can decrypt the token and it makes sense
+
+    return_val= False
+    try:
+        decrypted_jwt_token = decode(jwt_token)
+        if decrypted_jwt_token['scope']:
+            for user_scope_list in decrypted_jwt_token['scope']:
+                if user_scope_list == scope_type:
+                    app.logger.debug('Valid JWT scope.')
+                    return_val=True
+
+        if not return_val:
+            app.logger.warning('Invalid JWT scope.')
+            return False
+
+        if decrypted_jwt_token['expires_at']:
+            # We have a time stamp so check this token has not expired
+            #TODO Add UTC Time stamp validation
+            app.logger.info('Token: {} has a UTC time stamp of: {}'.format(decrypted_jwt_token['access_token'],decrypted_jwt_token['expires_at']))
+        else:
+            # We don't have a time stamp
+            app.logger.warning('Token has expired for token Value: {}'.format(decrypted_jwt_token['access_token']))
+            return False
+
+        return return_val
+
+    except JWTError:
+        app.logger.warning('JWT scope could not be validated.')
+        return False
+
+    except KeyError:
+        app.logger.warning('JWT scope could not be validated.')
+        return False
 
 
 @app.route('/enrolment-codes/<string:enrolment_code>', methods=['GET'])
